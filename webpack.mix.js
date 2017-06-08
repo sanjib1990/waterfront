279cd258ed39525f3070e48e2a6010d4691361e7
sanjib <devnath.sanjib@gmail.com>
2017-06-08 22:36:27 +0530

    New Changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const { mix } = require('laravel-mix');
  */
 
 mix
-    .js([
+    .scripts([
         'resources/assets/js/app.js'
     ], 'public/js/app.js')
     .sass('resources/assets/sass/app.scss', 'public/css/app.css')
