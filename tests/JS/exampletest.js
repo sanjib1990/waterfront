2134ab5874373db0f39a02f7e4a3b42149d5f537
Sanjib Devnath <devnath.sanjib@gmail.com>
2017-06-27 14:10:01 +0530

    NPM Test

---
+++ tests/JS/exampletest.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sanjibdevnath on 27/6/17.
+ */
+
+var expect  = require("chai").expect;
+
+describe("Test", function () {
+   it("should Do a simple test", function () {
+       expect(4+8).equal(12);
+   })
+});
